9ff8357024456bf7df0dfdf5114fba5c6b5439bf
Getch-27 <getahunlast@gmail.com>
2024-11-05 10:05:44 +0300

    refactor : change array list to handle conected nodes to dictionary

---
+++ graph.py
@@ -0,0 +1,26 @@
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        if v not in self.graph:
+            self.graph[v] = []
+        
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+        
+    def display(self):
+        for node, neighbors in self.graph.items():
+            print(f"Node {node} is connected to: {', '.join(map(str, neighbors))}")
+
+
+
+
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 3)
+g.display()
