22f7cf0b70ecc849f83d67454fd2487e1c2356be
Getch-27 <getahunlast@gmail.com>
2024-11-05 10:32:17 +0300

    feat : parse cites

--- graphdataStructure.py
+++ graphdataStructure.py
@@ -15,12 +15,21 @@ class Graph:
         for node, neighbors in self.graph.items():
             print(f"Node {node} is connected to: {', '.join(map(str, neighbors))}")
 
+def parse_graph(textFile):
+    graph = Graph()
+    with open (textFile, 'r') as file:
+        for line in file:
+            source, destination = line.strip().split(',')
+            graph.add_edge(source, destination)
+    return graph
+        
+graph = parse_graph('cities.txt')
+graph.display()
 
 
-
-g = Graph()
-g.add_edge(0, 1)
-g.add_edge(0, 2)
-g.add_edge(1, 2)
-g.add_edge(2, 3)
-g.display()
+# g = Graph()
+# g.add_edge(0, 1)
+# g.add_edge(0, 2)
+# g.add_edge(1, 2)
+# g.add_edge(2, 3)
+# g.display()
