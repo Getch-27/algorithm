22ff679ce5747a2a965f3eaf504e2c2d2fc5a7eb
Getch-27 <getahunlast@gmail.com>
2024-11-05 11:23:32 +0300

    refactor:change the graph class to be as datastructure only class

--- graphdataStructure.py
+++ graphdataStructure.py
@@ -1,6 +1,7 @@
 class Graph:
-    def __init__(self):
+    def __init__(self, textFile='cities.txt'):
         self.graph = {}
+        self.parse_graph(textFile)
 
     def add_edge(self, u, v):
         if u not in self.graph:
@@ -10,21 +11,25 @@ class Graph:
         
         self.graph[u].append(v)
         self.graph[v].append(u)
-        
+
     def display(self):
         for node, neighbors in self.graph.items():
             print(f"Node {node} is connected to: {', '.join(map(str, neighbors))}")
 
-def parse_graph(textFile):
-    graph = Graph()
-    with open (textFile, 'r') as file:
-        for line in file:
-            source, destination = line.strip().split(',')
-            graph.add_edge(source, destination)
-    return graph
-        
-graph = parse_graph('cities.txt')
-graph.display()
+    def parse_graph(self, textFile):
+        try:
+            with open(textFile, 'r') as file:
+                for line in file:
+                    source, destination = line.strip().split(',')
+                    self.add_edge(source, destination)
+        except FileNotFoundError:
+            print(f"Error: File '{textFile}' not found.")
+    
+    
+
+   
+# graph = parse_graph('cities.txt')
+# graph.visualize()
 
 
 # g = Graph()
