7d3919aa7b0b79637743e0ad641108564f4a4c18
Getch-27 <getahunlast@gmail.com>
2024-11-06 07:38:37 +0300

    feat :  add documentation to BFS

--- BFS.py
+++ BFS.py
@@ -2,6 +2,45 @@ from graphdataStructure import Graph
 from collections import deque
 
 def BFS(graph, start_node, target_node):
+    """
+    Performs a Breadth-First Search (BFS) on a graph to find the shortest path from a start node 
+    to a target node.
+
+    This function explores all the nodes in the graph level by level, starting from the `start_node`. 
+    It maintains a queue to keep track of the nodes to be explored and a distance map to store the 
+    distance from the start node to each node. If the target node is found, it traces back the path 
+    from the target node to the start node using parent pointers and prints the shortest path and traversal order.
+    
+    Parameters:
+        graph (Graph): An instance of the `Graph` class containing the graph data structure.
+        start_node (str): The starting node for the BFS traversal.
+        target_node (str): The target node to be found during the traversal.
+    
+    Returns:
+        list: A list representing the shortest path from `start_node` to `target_node` if reachable, 
+              otherwise `None`.
+    
+    Example:
+        graph = Graph()
+        BFS(graph, "Miami", "San Francisco")
+    
+    Output:
+        If the target node is reachable from the start node:
+        - Prints the shortest path found from start to target node.
+        - Prints the order in which nodes were visited during the BFS traversal.
+    
+        If the target node is not reachable:
+        - Prints a message indicating that the target node is not reachable.
+        - Prints the order in which nodes were visited during the BFS traversal.
+    
+    Time Complexity:
+        - The BFS algorithm visits every node and edge in the graph. Thus, the time complexity is O(V + E),
+          where V is the number of vertices (nodes) and E is the number of edges in the graph.
+    
+    Space Complexity:
+        - The space complexity is O(V), as we store color, distance, parent, and the queue, all of which 
+          depend on the number of nodes in the graph.
+    """
     color, distance, parent = {}, {}, {}
     
     for node in graph.graph:
@@ -10,7 +49,7 @@ def BFS(graph, start_node, target_node):
         parent[node] = None
 
         
-    node_order =[]
+    node_order =[] 
     color[start_node] = 'gray'
     distance[start_node] = 0
     queue = deque([start_node])
@@ -48,6 +87,7 @@ def BFS(graph, start_node, target_node):
     print("Target node not reachable from start node.")
     print("Traversal Order: " + " --> ".join(node_order))  
     return None
+
 
 # Example usage
 graph = Graph()
